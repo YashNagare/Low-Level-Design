89f2bd7a9640a2a97c5c9f1c8f627dec80b5526f
YashNagare <yashpnagare@gmail.com>
2025-07-05 19:45:02 +0530

    added Design Vending Machine

---
+++ src/VendingMachine/states/HasMoneyState.java
@@ -0,0 +1,34 @@
+package VendingMachine.states;
+
+import VendingMachine.Coin;
+import VendingMachine.VendingMachine;
+
+public class HasMoneyState extends State {
+
+    public HasMoneyState(VendingMachine machine) {
+        super(machine);
+    }
+
+    @Override
+    public void insertCoin(Coin coin) {
+        System.out.println("Already received full amount.");
+    }
+
+    @Override
+    public void selectItem(String code) {
+        System.out.println("Item already selected.");
+    }
+
+    @Override
+    public void dispense() {
+        machine.setState(new DispensingState(machine));
+        machine.dispenseItem();
+    }
+
+    @Override
+    public void refund() {
+        machine.refundBalance();
+        machine.reset();
+        machine.setState(new IdleState(machine));
+    }
+}
---
+++ src/VendingMachine/states/State.java
@@ -0,0 +1,18 @@
+package VendingMachine.states;
+
+import VendingMachine.Coin;
+import VendingMachine.VendingMachine;
+
+public abstract class State {
+
+    VendingMachine machine;
+
+    State(VendingMachine machine) {
+        this.machine = machine;
+    }
+
+    public abstract void insertCoin(Coin coin);
+    public abstract void selectItem(String code);
+    public abstract void dispense();
+    public abstract void refund();
+}
