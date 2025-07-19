eb0869963754b6de00b28d3757bf378a8ae7edca
YashNagare <yashpnagare@gmail.com>
2025-07-19 19:11:01 +0530

    added Decorator Design Pattern

---
+++ src/DesignPatterns/Structural/Decorator/Coffee.java
@@ -0,0 +1,6 @@
+package DesignPatterns.Structural.Decorator;
+
+public interface Coffee {
+    String getDescription();
+    double getCost();
+}
---
+++ src/DesignPatterns/Structural/Decorator/CoffeeDecorator.java
@@ -0,0 +1,16 @@
+package DesignPatterns.Structural.Decorator;
+
+public abstract class CoffeeDecorator implements Coffee {
+    protected Coffee decoratedCoffee;
+    public CoffeeDecorator(Coffee coffee) {
+        this.decoratedCoffee = coffee;
+    }
+
+    public String getDescription() {
+        return decoratedCoffee.getDescription();
+    }
+
+    public double getCost() {
+        return decoratedCoffee.getCost();
+    }
+}
